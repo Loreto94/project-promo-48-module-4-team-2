76aeea0addc86757dddc017b01a2bdb471730b59
laiapozo <pozotalarnlaia@gmail.com>
2025-04-10 12:30:45 +0200

    Create database

---
+++ src/services/defaultdb.sql
@@ -0,0 +1,21 @@
+USE defaultdb;
+
+CREATE TABLE projectsData (
+	idProject INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(45) NOT NULL,
+    slogan VARCHAR(150) NOT NULL,
+    repo VARCHAR(250) NOT NULL,
+    demo VARCHAR(250) NOT NULL,
+    technologies VARCHAR(150) NOT NULL,
+    description TINYTEXT NOT NULL,
+    image VARCHAR(250) NOT NULL
+);
+
+CREATE TABLE userData (
+	idUser INT AUTO_INCREMENT PRIMARY KEY,
+    autor VARCHAR(45) NOT NULL,
+    job VARCHAR(45) NOT NULL,
+    photo VARCHAR(250) NOT NULL
+);
+
+SELECT * FROM projectsData, userData;
